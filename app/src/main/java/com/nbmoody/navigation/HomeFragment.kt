74a158b4986aa0c652b85886c2e31a58ccf251ed
Ben Moody <nathanielmoody@live.com>
2019-03-28 15:01:58 -0400

    Wire up first use of navigation graph, from HomeFragment to FlowStepFragment.

--- app/src/main/java/com/nbmoody/navigation/HomeFragment.kt
+++ app/src/main/java/com/nbmoody/navigation/HomeFragment.kt
@@ -18,7 +18,10 @@ package com.nbmoody.navigation
 
 import android.os.Bundle
 import android.view.*
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.navOptions
 
 /**
  * Fragment used to show how to navigate to another destination
@@ -36,12 +39,20 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        //TODO STEP 5 - Set an OnClickListener, using Navigation.createNavigateOnClickListener()
-//        val button = view.findViewById<Button>(R.id.navigate_destination_button)
-//        button?.setOnClickListener {
-//            findNavController().navigate(R.id.flow_step_one_dest)
-//        }
-        //TODO END STEP 5
+        val navigateOptions = navOptions {
+            anim {
+                enter = R.anim.slide_in_right
+                exit = R.anim.slide_out_left
+                popEnter = R.anim.slide_in_left
+                popExit = R.anim.slide_out_right
+            }
+        }
+        val button = view.findViewById<Button>(R.id.navigate_destination_button)
+        button?.setOnClickListener {
+            findNavController().navigate(R.id.flow_step_one_dest, null, navigateOptions)
+        }
+
+
 
         //TODO STEP 6 - Set NavOptions
 //        val options = navOptions {
